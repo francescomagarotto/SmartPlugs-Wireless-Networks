ffb698711cfede0832c88e133b4d74c9509d07a1
UdrK <nomefittizioqualunque@gmai.com>
2021-01-17 14:57:28 +0100

    implemented client and finished implementing server

--- src/main/java/it/unipd/wirelessnetworks/client/SmartPlugsClients.java
+++ src/main/java/it/unipd/wirelessnetworks/client/SmartPlugsClients.java
@@ -1,29 +1,123 @@
 package it.unipd.wirelessnetworks.client;
 
 import org.json.JSONObject;
-
-import java.lang.Math;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.PriorityQueue;
+import java.lang.Math;
 
-public class SmartPlugsClients {
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutorService;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class SmartPlugsClients extends WebSocketClient{
+
+    private String id, name, watts;
+
+    public SmartPlugsClients(URI serverUri, Draft draft, String id, String name, String watts) {
+        super(serverUri, draft);
+        this.id = id;
+        this.name = name;
+        this.watts = watts;
+	}
+
+	public SmartPlugsClients(URI serverURI, String id, String name, String watts) {
+        super(serverURI);
+        this.id = id;
+        this.name = name;
+        this.watts = watts;
+	}
+
+	@Override
+	public void onOpen(ServerHandshake handshakedata) {
+		send("{'id':'"+id+"', 'name': '"+name+"', 'watts': '"+watts+"'}");
+		System.out.println("[CLIENT-"+id+"] Connected to server, waiting for response...");
+	}
+
+	@Override
+	public void onClose(int code, String reason, boolean remote) {
+		System.out.println("[CLIENT-"+id+"] Closed connection with exit code " + code + " additional info: " + reason);
+	}
+
+	@Override
+	public void onMessage(String message) {
+        JSONObject obj = new JSONObject(message);
+        String messageId = obj.getString(id);
+        String type = obj.getString("type");
+        // if the message sent from the server is to this client
+        if(messageId.equals(id)) {  
+            switch(type) {
+                case "OK":
+                    // server recognizes on its own the client is already connected and disconnects it
+                    try {
+                        Thread.sleep((int)(Math.random()*10000));
+                    } catch (Exception e) {
+                        System.out.println("[CLIENT-"+id+"] Something went wrong while connected to grid "+e.getMessage());
+                    }
+                    
+                    send("{'id':'"+id+"', 'name': '"+name+"', 'watts': '"+watts+"'}");
+                    break;
+                case "EXCEEDED":
+                    try {
+                        Thread.sleep((int)(Math.random()*10000));
+                    } catch (Exception e) {
+                        System.out.println("[CLIENT-"+id+"] Something went wrong while waiting to retry to connect "+e.getMessage());
+                    }
+                    send("{'id':'"+id+"', 'name': '"+name+"', 'watts': '"+watts+"'}");
+                    break;
+                case "DISCONNECT":
+                    close();
+                    break;
+            }
+        }
+	}
+
+	@Override
+	public void onMessage(ByteBuffer message) {
+		System.out.println("[CLIENT-"+id+"] Received message in an unsupported format");
+	}
+
+	@Override
+	public void onError(Exception ex) {
+		System.err.println("[CLIENT-"+id+"] An error occurred:" + ex);
+	}
 
     public static void main(String[] args) {
+        String[] names = {"dishwasher", "fridge", "washing machine", "tv", "computer", "phon", "oven", "microwave oven", "electric stove", "vacuum"};
 
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-        executorService.submit(
-            new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        // preparing message to send to the server
+        for(int i=0; i<10; i++) {
+            
+            
+            executorService.execute(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String id = String.valueOf(Thread.currentThread().getId());
+                            String name = names[(int)(Math.random()*10)];
+                            String watts = String.valueOf((int)(Math.random()*2000)+1);
+                            WebSocketClient client = new SmartPlugsClients(new URI("ws://localhost:8887"), id, name, watts);
+                            client.connect();
+                        } catch (Exception e) { 
+                            e.printStackTrace(); 
+                        }
+                    }
+                }
+            );
+        }
+
+    }
+}
+
+/*
+// preparing message to send to the server
                         JSONObject obj = new JSONObject();
                         int watt = (int)(Math.random()*2000)+1; // between 1 and 2000
                         obj.put("watt", Integer.valueOf(watt));
@@ -56,12 +150,4 @@ public class SmartPlugsClients {
                         inputStream.close();
                         outputStream.close();
                         socket.close();
-
-                    } catch (IOException e) { 
-                        e.printStackTrace(); 
-                    }
-                }
-            }
-        );
-    }
-}
+*/
--- src/main/java/it/unipd/wirelessnetworks/server/SmartPlugsServer.java
+++ src/main/java/it/unipd/wirelessnetworks/server/SmartPlugsServer.java
@@ -22,7 +22,7 @@ public class SmartPlugsServer extends WebSocketServer {
         clients = new Hashtable<String, ArrayList<String>>();
     }
 
-    private void connectClient(WebSocket connection, String clientID, String clientName, String clientWatts) {
+    private void connectClientToGrid(WebSocket connection, String clientID, String clientName, String clientWatts) {
         // updating count of used watts
         wattCounter.addAndGet(Integer.parseInt(clientWatts));
 
@@ -36,7 +36,7 @@ public class SmartPlugsServer extends WebSocketServer {
         connection.send("{'type' : 'OK', 'next': 60}");
     }
 
-    private void disconnectClient(String clientID) {
+    private void disconnectClientFromGrid(String clientID) {
         broadcast("{'type': 'DISCONNECT', id: "+clientID+"}");
         int clientWatts = Integer.parseInt(clients.get(clientID).get(1));
         wattCounter.addAndGet(-clientWatts);
@@ -45,93 +45,103 @@ public class SmartPlugsServer extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket connection, ClientHandshake handshake) {
-        String clientID = handshake.getFieldValue("id");
-        String clientName = handshake.getFieldValue("name");
-        String clientWatts = handshake.getFieldValue("watts");
-
-        // if there's enough wattage to connect client
-        if (wattCounter.get() + Integer.parseInt(clientWatts) <= MAX_WATT_COUNTER) {
-            connectClient(connection, clientID, clientName, clientWatts);
-        } else {
-            System.out.println("[SERVER]:");
-            System.out.println(clientName+" has tried to connect but there's not enough wattage to accomodate it");
-            System.out.println("Type the IDs separated by spaces of the devices to disconnect in order to accomodate "+clientName+" or Type -1 to leave "+clientName+"disconnected");
-            // listing connected clients for the user 
-            for(String key : clients.keySet()) {
-                System.out.println("Id: "+key+" device: "+clients.get(key).get(0)+" wattage: "+clients.get(key).get(1));
-            }
-
-            // getting enough clients to disconnect in order to connect new client
-            // if user doesn't want to disconnect other clients, telling new client it can't connect
-
-            int sumWattage = -1;    // starting from -1 in order to avoid printing to the user that the clients chosen aren't enough
-            ArrayList<String> idsToDisconnect = new ArrayList<>();
-
-            // while the user doesn't choose enough clients to disconnect
-            while(sumWattage < Integer.parseInt(clientWatts)) {
-
-                // this will be displayed only if the user tried to disconnect a few clients but weren't enough to accomodate the new client
-                if (sumWattage == 0) {
-                    System.out.println("The clients you chose to disconnect aren't enough.");
-                    System.out.println("Type the IDs separated by spaces of the devices to disconnect in order to accomodate "+clientName+" or Type -1 to leave "+clientName+"disconnected");
-                }
-
-                sumWattage = 0;
-                idsToDisconnect = new ArrayList<>();
-
-                // user input
-                Scanner scanner = new Scanner(System.in);
-                String inputString = scanner.nextLine();
-
-                if (inputString.equals("-1")) {
-                    // if we are here, the user doesn't want to disconnect other devices
-                    connection.send("{'type': 'EXCEEDED', id: "+clientID+"}");
-                } else {
-                    String[] ids = inputString.split(" ");
-                    for(int i=0; i<ids.length; i++) {
-                        if(!clients.keySet().contains(ids[i])) {
-                            System.out.println(ids[i]+"is not a valid id for a device, retry id insertion: ");
-                            sumWattage = 0; // makes sure the user has to input the ids again
-                            break;
-                        } else {
-                            sumWattage += Integer.parseInt(clients.get(ids[i]).get(1));
-                            idsToDisconnect.add(ids[i]);
-                        }
-                    }
-                }
-            }
-
-            // if we are here, the user gave a correct list of devices to disconnect to accomodate the new device
-            // disconnecting devices:
-            for(int i=0; i<idsToDisconnect.size();i++) {
-                disconnectClient(idsToDisconnect.get(i));
-            }
-
-            // connecting client
-            connectClient(connection, clientID, clientName, clientWatts);
-        }
+        System.out.println("[SERVER] Client "+connection.getRemoteSocketAddress()+" connected to the server");
     }
 
 	@Override
 	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        System.out.println("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
+        System.out.println("[SERVER] Closed connection with" + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
 	}
 
     @Override
-	public void onMessage(WebSocket conn, String message) {
-        System.out.println("received message from "	+ conn.getRemoteSocketAddress());
-        // handle messages from clients (will there even be?)
+	public void onMessage(WebSocket connection, String message) {
+        JSONObject obj = new JSONObject(message);
+        String clientID = obj.getString("id");
+        String clientName = obj.getString("name");
+        String clientWatts = obj.getString("watts");
+
+        if(clients.keySet().contains(clientID)) {
+            // we are here the message that raised onMessage came from a client which IS connected to the power grid
+            disconnectClientFromGrid(clientID);
+        } else {
+            // we are here the message that raised onMessage came from a client which ISN'T connected to the power grid
+            // if there's enough wattage to connect client
+            if (wattCounter.get() + Integer.parseInt(clientWatts) <= MAX_WATT_COUNTER) {
+                connectClientToGrid(connection, clientID, clientName, clientWatts);
+            } else {
+                System.out.println("[SERVER]:");
+                System.out.println(clientName+" has tried to connect but there's not enough wattage to accomodate it");
+                System.out.println("Type the IDs separated by spaces of the devices to disconnect in order to accomodate "+clientName+" or Type -1 to leave "+clientName+"disconnected");
+                // listing connected clients for the user 
+                for(String key : clients.keySet()) {
+                    System.out.println("Id: "+key+" device: "+clients.get(key).get(0)+" wattage: "+clients.get(key).get(1));
+                }
+
+                // getting enough clients to disconnect in order to connect new client
+                // if user doesn't want to disconnect other clients, telling new client it can't connect
+
+                int sumWattage = -1;    // starting from -1 in order to avoid printing to the user that the clients chosen aren't enough
+                ArrayList<String> idsToDisconnect = new ArrayList<>();
+
+                // while the user doesn't choose enough clients to disconnect
+                while(sumWattage < Integer.parseInt(clientWatts)) {
+
+                    // this will be displayed only if the user tried to disconnect a few clients but weren't enough to accomodate the new client
+                    if (sumWattage == 0) {
+                        System.out.println("The clients you chose to disconnect aren't enough.");
+                        System.out.println("Type the IDs separated by spaces of the devices to disconnect in order to accomodate "+clientName+" or Type -1 to leave "+clientName+"disconnected");
+                    }
+
+                    sumWattage = 0;
+                    idsToDisconnect = new ArrayList<>();
+
+                    // user input
+                    Scanner scanner = new Scanner(System.in);
+                    String inputString = scanner.nextLine();
+
+                    if (inputString.equals("-1")) {
+                        // if we are here, the user doesn't want to disconnect other devices
+                        connection.send("{'type': 'EXCEEDED', id: "+clientID+"}");
+                    } else {
+                        String[] ids = inputString.split(" ");
+                        for(int i=0; i<ids.length; i++) {
+                            if(!clients.keySet().contains(ids[i])) {
+                                System.out.println(ids[i]+"is not a valid id for a device, retry id insertion: ");
+                                sumWattage = 0; // makes sure the user has to input the ids again
+                                break;
+                            } else {
+                                sumWattage += Integer.parseInt(clients.get(ids[i]).get(1));
+                                idsToDisconnect.add(ids[i]);
+                            }
+                        }
+                    }
+                }
+
+                // if we are here, the user gave a correct list of devices to disconnect to accomodate the new device
+                // disconnecting devices:
+                for(int i=0; i<idsToDisconnect.size();i++) {
+                    disconnectClientFromGrid(idsToDisconnect.get(i));
+                }
+
+                // connecting client
+                connectClientToGrid(connection, clientID, clientName, clientWatts);
+            }
+
+
+        }
+
+
     }
     
 	@Override
-	public void onMessage( WebSocket conn, ByteBuffer message ) {
-        System.out.println("received message from "	+ conn.getRemoteSocketAddress());
+	public void onMessage( WebSocket connection, ByteBuffer message ) {
+        System.out.println("[SERVER] Received message in an unsupported format from " + connection.getRemoteSocketAddress());
 		// handle messages from clients (will there even be?)
 	}
 
 	@Override
-	public void onError(WebSocket conn, Exception ex) {
-		System.err.println("[SERVER] an error occurred on connection " + conn.getRemoteSocketAddress()  + ":" + ex);
+	public void onError(WebSocket connection, Exception e) {
+		System.err.println("[SERVER] An error occurred on connection " + connection.getRemoteSocketAddress()  + ":" + e);
 	}
 
     @Override
