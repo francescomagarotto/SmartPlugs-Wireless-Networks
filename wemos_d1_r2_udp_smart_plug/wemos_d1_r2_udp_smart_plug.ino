db560f3933bc510a54cda88012a68ec6e3b8d524
Francesco Magarotto <magarotto.francesco@gmail.com>
2021-02-27 18:12:29 +0100

    Added timer
    To be checked

--- wemos_d1_r2_udp_smart_plug/wemos_d1_r2_udp_smart_plug.ino
+++ wemos_d1_r2_udp_smart_plug/wemos_d1_r2_udp_smart_plug.ino
@@ -1,19 +1,28 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
 #include <WiFiUdp.h>
+#include <arduino-timer.h>
 #include <SoftwareSerial.h>
 #include <ModbusMaster.h>
-SoftwareSerial pzem(D5,D6);
+
+auto timer = timer_create_default();
+
+
+SoftwareSerial pzem(D5, D6);
 ModbusMaster node;// (RX,TX) connect to TX,RX of PZEM for NodeMCU
+
+
 #define RELAY D7
+
 const char* ssid = "Fritz! Network";
 const char* password = "Oliver2016Artu2015";
-double voltage_usage, current_usage, active_power, active_energy, frequency, power_factor, over_power_alarm; 
+
+volatile double max_usage = 0.0;
+char* remoteIP = NULL;
 uint8_t result; uint16_t data[6];
 WiFiUDP Udp;
 unsigned int localUdpPort = 4210;
 char incomingPacket[256];
-char replyPacket[] = "Hi there! Got the message :-)";
 void setup() {
   Serial.begin(115200);
   pzem.begin(9600);
@@ -34,9 +43,15 @@ void setup() {
 }
 
 void loop() {
+  timer.tick();
+  timer.every(1000, check);
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
+    if(remoteIP == NULL) {
+      remoteIP = new char[16];
+      Udp.remoteIP().toString().toCharArray(remoteIP, 16);
+    }
     // receive incoming UDP packets
     Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
     int len = Udp.read(incomingPacket, 255);
@@ -49,32 +64,21 @@ void loop() {
     DeserializationError error = deserializeJson(doc, incomingPacket);
     requestStrategy(doc.as<JsonObject>(), Udp.remoteIP(), Udp.remotePort());
     // send back a reply, to the IP address and port we got the packet from
-    pzemdata();
-    }
+  }
 }
-void pzemdata(){
-
-    result = node.readInputRegisters(0x0000, 10);
-    
-    if (result == node.ku8MBSuccess)  
-    {
+void pzemdata(double& active_power, double& active_energy) {
+  double voltage_usage, current_usage, frequency, power_factor, over_power_alarm;
+  result = node.readInputRegisters(0x0000, 10);
+  if (result == node.ku8MBSuccess)
+  {
     voltage_usage      = (node.getResponseBuffer(0x00) / 10.0f);
     current_usage      = (node.getResponseBuffer(0x01) / 1000.000f);
-    active_power       = (node.getResponseBuffer(0x03) / 10.0f);
-    active_energy      = (node.getResponseBuffer(0x05) / 1000.0f);
+    active_power     = (node.getResponseBuffer(0x03) / 10.0f);
+    active_energy    = (node.getResponseBuffer(0x05) / 1000.0f);
     frequency          = (node.getResponseBuffer(0x07) / 10.0f);
     power_factor       = (node.getResponseBuffer(0x08) / 100.0f);
     over_power_alarm   = (node.getResponseBuffer(0x09));
-    }
-    Sr
-  Serial.print("VOLTAGE:           ");   Serial.println(voltage_usage);   // V
-  Serial.print("CURRENT_USAGE:     ");   Serial.println(current_usage, 3);  //  A
-  Serial.print("ACTIVE_POWER:      ");   Serial.println(active_power);   //  W
-  Serial.print("ACTIVE_ENERGY:     ");   Serial.println(active_energy, 3);  // kWh
-  Serial.print("FREQUENCY:         ");   Serial.println(frequency);    // Hz
-  Serial.print("POWER_FACTOR:      ");   Serial.println(power_factor);
-  Serial.print("OVER_POWER_ALARM:  ");   Serial.println(over_power_alarm, 0);
-  Serial.println("====================================================");
+  }
 }
 
 void requestStrategy(const JsonObject& jsonDocument, const IPAddress& remoteIP, const int remotePort) {
@@ -82,16 +86,18 @@ void requestStrategy(const JsonObject& jsonDocument, const IPAddress& remoteIP, 
   JsonObject obj = doc.createNestedObject();
   const char* s = jsonDocument["act"];
   if (strcmp(s, "INIT") == 0) {
-    obj["act"] = "ACK";
+    obj["act"] = "INIT";
+    obj["type"] = "DISHWASHER";
+    obj["max_power_usage"] = max_usage;
     obj["sts"] = digitalRead(RELAY);
   }
   else if (strcmp(s, "ON") == 0) {
-    obj["act"] = "ACK";
+    obj["act"] = "ON";
     digitalWrite(RELAY, HIGH);
     obj["sts"] = digitalRead(RELAY);
-    }
+  }
   else if (strcmp(s, "OFF") == 0) {
-    obj["act"] = "ACK";
+    obj["act"] = "OFF";
     digitalWrite(RELAY, LOW);
     obj["sts"] = digitalRead(RELAY);
   }
@@ -99,3 +105,16 @@ void requestStrategy(const JsonObject& jsonDocument, const IPAddress& remoteIP, 
   serializeJson(doc, Udp);
   Udp.endPacket();
 }
+
+bool check(void*) {
+  double active_power, active_energy;
+  pzemdata(active_power, active_energy);
+  if(remoteIP != NULL && active_power < max_usage || active_power > max_usage) {
+      StaticJsonDocument<200> doc;
+      JsonObject obj = doc.createNestedObject();
+      Udp.beginPacket(remoteIP, localUdpPort);
+      serializeJson(doc, Udp);
+      Udp.endPacket();
+  }
+  return true;
+  }
